06f2eb7836aeaa1b0c2b37291f69fc0c121310fa
nickholo <hologron@sheridancollege.ca>
2024-09-27 15:15:35 -0400

    Completed angular directives example app

--- angularDirectives/src/app/directives01/directives01.component.ts
+++ angularDirectives/src/app/directives01/directives01.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import Student from '../classStudent';
 
 @Component({
-  selector: 'app-directives01',
-  templateUrl: './directives01.component.html',
-  styleUrl: './directives01.component.css'
+	selector: 'app-directives01',
+	templateUrl: './directives01.component.html',
+	styleUrl: './directives01.component.css',
 })
 export class Directives01Component {
-
+	@Input() personalData!: Student;
+	loginCheck: string = 'hologron@sheridancollege.ca';
+	clearLogin() {
+		this.loginCheck = '';
+	}
 }
--- angularDirectives/src/app/directives02/directives02.component.ts
+++ angularDirectives/src/app/directives02/directives02.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import Student from '../classStudent';
+
+class Course {
+	ccode!: string;
+	cname!: string;
+	credits!: number;
+}
 
 @Component({
-  selector: 'app-directives02',
-  templateUrl: './directives02.component.html',
-  styleUrl: './directives02.component.css'
+	selector: 'app-directives02',
+	templateUrl: './directives02.component.html',
+	styleUrl: './directives02.component.css',
 })
 export class Directives02Component {
+	@Input() personalData!: Student;
+	@Input() testData!: Student;
 
+	city: string = 'Brampton';
+
+	cdate = new Date();
+
+	courseList: Course[] = [
+		{ ccode: 'SYST24444', cname: 'Mobile Web', credits: 3 },
+		{ ccode: 'PROG32356', cname: '.NET C#', credits: 6 },
+		{ ccode: 'INFO20172', cname: 'Project Mgt', credits: 3 },
+		{ ccode: 'PROG20799', cname: 'Data Structures', credits: 6 },
+	];
 }
