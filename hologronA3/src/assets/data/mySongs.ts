48b69da8150cebcb366548cc01ea01be0c05783c
nickholo <hologron@sheridancollege.ca>
2024-11-06 15:32:29 -0500

    Finished assignment 3

---
+++ hologronA3/src/assets/data/mySongs.ts
@@ -0,0 +1,32 @@
+import Song from '../../app/songInfo';
+
+export const songList: Song[] = [
+	{
+		name: 'Dear Maria, Count Me In',
+		artist: 'All Time Low',
+		genre: 'Alternative',
+		yearReleased: 2008,
+		picture: 'allTimeLow.jpeg',
+	},
+	{
+		name: 'Harness Your Hopes',
+		artist: 'Pavement',
+		genre: 'Alternative Rock',
+		yearReleased: 1999,
+		picture: 'pavement.jpeg',
+	},
+	{
+		name: 'Here Is No Why',
+		artist: 'The Smashing Pumpkins',
+		genre: 'Alternative Rock',
+		yearReleased: 1995,
+		picture: 'mellonCollieInfiniteSadness.jpeg',
+	},
+	{
+		name: "Adam's Song",
+		artist: 'Blink-182',
+		genre: 'Emo / Pop Punk / Alternative Rock',
+		yearReleased: 2000,
+		picture: 'enemaOfTheState.jpeg',
+	},
+];
